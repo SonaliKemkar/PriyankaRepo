c67b2edd403cd95ec9695459ec39d21aa9c42ff5
SonaliKemkar <50901509+SonaliKemkar@users.noreply.github.com>
2019-11-11 15:03:21 +0530

    Update runscope-app.py

--- runscope-app.py
+++ runscope-app.py
@@ -1,7 +1,7 @@
 import requests
 
 def testStart(test_id):
-	return requests.post('https://a.blazemeter.com/api/v4/tests/7389741/start'.format(test_id), auth=('id','secret'))
+	return requests.post('https://a.blazemeter.com/api/v4/tests/7389741/start'.format(test_id), auth=('8cac46d7b462618937db5b75','83b720f2e2c405866b1769bb4582635542a684e548331a948308e2f89ccf7ec0807c28db'))
 
 res = testStart(7389741)
 print res.json()
